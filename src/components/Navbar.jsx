eb79d64cf68ff0b4429355a5857d0ab7817e32d8
Prajwal Rajan Ladkat <praju.ladkat@gmail.com>
2023-11-01 07:42:00 +0530

    added logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import Logo from "./Logo"
 const Navbar = () => {
 
     return (
-        <nav className='absolute z-10 w-screen py-6 top-0 flex items-center justify-between px-8 lg:justify-evenly'>
+        <nav className='absolute z-10 w-screen py-6 top-0 flex items-center px-5 justify-between'>
            {/* logo */}
-           <Logo />
-            <div className="hidden md:inline-block">
+          <div>
+          <Logo />
+          </div>
+            <div className="hidden md:inline-flex space-x-4">
                 <ul className='flex justify-center'>
                     {menuItems.map((item) => (
                         <a href={`#${item.name.toLowerCase()}`} key={item.id} className='mx-5 text-lg font-bold hover:bg-white hover:text-black cursor-pointer px-3 py-2 rounded-lg duration-200 transition-all text-white uppercase'>
@@ -15,9 +17,12 @@ const Navbar = () => {
                         </a>
                     ))}
                 </ul>
+                <div>
+                <button className="bg-fuchsia-600 rounded-md hover:bg-purple-600 duration-200 ease-linear hover:scale-105 text-white font-bold text-lg px-3 py-2 ">Contact Us</button>
+            </div>
             </div>
             <div>
-                <button className="bg-fuchsia-600 rounded-md hover:bg-purple-600 duration-200 ease-linear hover:scale-105 text-white font-bold text-lg px-3 py-2 ">Contact Us</button>
+                <button className="bg-fuchsia-600 rounded-md hover:bg-purple-600 duration-200 ease-linear hover:scale-105 text-white font-bold text-base lg:text-lg px-2 py-2 ">Contact Us</button>
             </div>
         </nav>
     )
